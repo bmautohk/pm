8eba1118b3014fdb91aed89fdcc2721fb1fa2feb
rickykei <rickykei@yahoo.com.hk>
2012-10-12 22:55:00 +0800

    css layout change

--- pm/protected/views/layouts/main.php
+++ pm/protected/views/layouts/main.php
@@ -1,22 +1,17 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<? $baseUrl = Yii::app()->request->baseUrl;
-
-// Get selected made
-$session=new CHttpSession;
-$session->open();
-
-if (isset($session["SELECTED_MADE"])) {
-	$selectedMade = $session[GlobalConstants::SESSION_SELECTED_MADE];
-}
-else {
-	$selectedMade = "TOYOTA";
+<? 
+// Verify the user
+if (Yii::app()->user->getState('role') != 'USER') {
+	// User has not logged in yet, return to login page
+	$this->redirect(Yii::app()->homeUrl);
 }
 
-$session->close();
+$baseUrl = Yii::app()->request->baseUrl;
+$mades =  Made::getDropDownFromCache();
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=euc-jp" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
 
 	<link rel="stylesheet" type="text/css" href="<?=$baseUrl ?>/css/css.css" />
@@ -24,22 +19,21 @@ $session->close();
 	
 	<? Yii::app()->clientScript->registerCoreScript('jquery');?>
 
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	<title><?php echo CHtml::encode(Yii::app()->name); ?></title>
 </head>
 
-<body style="position: relative;background-color:;width:100%;">
+<body>
 	<div id="wrapper">
 		<div id="header_main"> 
-			<div id="welcome_msg">Welcome <?=Yii::app()->user->name ?></div>
+			<div id="welcome_msg">Welcome <?=Yii::app()->user->name ?> <a href="<?=$baseUrl ?>/site/logout" style="color:white; text-decoration:underline">(Logout)</a></div>
 			<div id="name_main">BM AUTO ACCESSORIES (HK) CO. LTD.</div>
 		</div>
 
 		<div id="body_left_main">
 			<div id="logo_main"><a href="<?=$baseUrl ?>/product"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logo.png"/></a></div>
-			<a class="<?=($selectedMade=="TOYOTA"?"menubar_selected" : "menubar") ?>" href="<?=$baseUrl ?>/product?made=TOYOTA">TOYOTA</a>
-			<a class="<?=($selectedMade=="HONDA"?"menubar_selected" : "menubar") ?>" href="<?=$baseUrl ?>/product?made=HONDA">HONDA</a>
-			<a class="<?=($selectedMade=="SUZUKI"?"menubar_selected" : "menubar") ?>" href="<?=$baseUrl ?>/product?made=SUZUKI">SUZUKI</a>
-			<a class="<?=($selectedMade=="MITSUBISHI"?"menubar_selected" : "menubar") ?>" href="<?=$baseUrl ?>/product?made=MITSUBISHI">MITSUBISHI</a>
+			<? foreach ($mades as $made) {?>
+				<a class="menubar" href="<?=$baseUrl ?>/product/searchByFilter?ProductSearchForm%5Bmade%5D=<?=$made ?>"><?=$made ?></a>
+			<? }?>
 		</div>
 		<div id="body_right_main">
 			<?php echo $content; ?>
