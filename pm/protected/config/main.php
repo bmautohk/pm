1b1672f616cf774bff5d0057bc024eecdc6265e8
Kelly <finalrain@gmail.com>
2015-04-06 16:59:00 +0800

    Control jquery version in main.php

--- pm/protected/config/main.php
+++ pm/protected/config/main.php
@@ -95,6 +95,18 @@ return array(
 		'excel'=>array(
 				'class'=>'application.extensions.PHPExcel',
 		),
+		'clientScript'=>array(
+				'packages'=>array(
+						'jquery'=>array(
+								'baseUrl'=>'js',
+								'js'=>array('jquery-1.7.2.min.js'),
+						),
+						'jquery-ui'=>array(
+								'baseUrl'=>'js',
+								'js'=>array('jquery-ui.min.js'),
+						)
+				),
+		),
 	),
 
 	// application-level parameters that can be accessed
